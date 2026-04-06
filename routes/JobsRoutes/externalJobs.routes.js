99f01388350de99f2a3eedf69ad69a14ec420501
Mangesh Rajguru <msrajguru7@gmail.com>
2026-04-06 23:00:03 +0530

    added existing routes

---
+++ routes/JobsRoutes/externalJobs.routes.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const jobsRouter=express.Router()
+
+//importing middleware
+const { protect } = require('../../middleware/authMiddleware');
+
+//importing controller
+const jobController = require('../../controllers/jobController');
+
+jobsRouter.get('/jobs/', protect, jobController.getJobs);
+
+module.exports={jobsRouter}
---
+++ routes/StudentRoutes/studentProfile.routes.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const userProfileRouter=express.Router()
+
+//importing middleware
+const { protect } = require('../../middleware/authMiddleware');
+
+// importing controller
+const userProfileController = require('../controllers/userProfileController');
+
+//profile routes
+userProfileRouter.get('/profile/', protect, userProfileController.getProfiles);
+userProfileRouter.post('/profile/', protect, userProfileController.createProfile);
+userProfileRouter.get('/profile/:id/', protect, userProfileController.getProfile);
+userProfileRouter.put('/profile/:id/', protect, userProfileController.updateProfile);
+userProfileRouter.delete('/profile/:id/', protect, userProfileController.deleteProfile);
+
+module.exports={userProfileRouter}
