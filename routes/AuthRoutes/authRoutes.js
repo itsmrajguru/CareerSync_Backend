e1d64fe2f62559c7fd2df8f5ecbff35d2d69a4f7
Mangesh Rajguru <msrajguru7@gmail.com>
2026-04-12 10:26:08 +0530

    added the RateLimiter functionality to preveent the website from brut-force attacks

--- routes/AuthRoutes/authRoutes.js
+++ routes/AuthRoutes/authRoutes.js
@@ -5,14 +5,15 @@ const authRouter=express.Router()
 const authController = require('../../controllers/Auth/authController');
 
 const { protect } = require('../../middleware/authMiddleware/authMiddleware');
+const { authRateLimiter } = require('../../middleware/rateLimiter/authRateLimiter');
 
 // User Authentication Routes
-authRouter.post('/signup', authController.signup);
-authRouter.post('/verify-otp', authController.verifySignupOtp); //verifies OTP sent during signup to complete registration
-authRouter.post('/login', authController.login);
+authRouter.post('/signup', authRateLimiter, authController.signup);
+authRouter.post('/verify-otp', authRateLimiter, authController.verifySignupOtp); //verifies OTP sent during signup to complete registration
+authRouter.post('/login', authRateLimiter, authController.login);
 authRouter.post('/token/refresh', authController.refreshToken);
-authRouter.post('/forgot-password', authController.forgotPassword);
-authRouter.post('/reset-password', authController.resetPassword);
+authRouter.post('/forgot-password', authRateLimiter, authController.forgotPassword);
+authRouter.post('/reset-password', authRateLimiter, authController.resetPassword);
 authRouter.post('/change-password', protect, authController.changePassword);
 authRouter.delete('/delete-me', protect, authController.deleteMe);
 authRouter.post('/logout', (req, res) => res.json({ success: true, message: 'Logged out' }));
