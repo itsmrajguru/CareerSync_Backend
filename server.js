6937d5f2d9171ab72ef6ebcb04ad9c93f5bd7814
Mangesh Rajguru <msrajguru7@gmail.com>
2026-06-07 11:35:54 +0530

    updated server with new frontend url

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ require('dnscache')({
 //cors for cross-origin platforms
 const allowedOrigins = [
     process.env.CLIENT_URL,
-    "https://careersyncplatform.vercel.app",
-    "https://careersyncc.netlify.app",
+    "https://careersyncapp.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
